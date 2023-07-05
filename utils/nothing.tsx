3712694f51ee452afb894de3dcfc17a90d87fe58
George-F-Ramzi <109841377+George-F-Ramzi@users.noreply.github.com>
2023-07-05 20:08:31 +0300

    Finished Nothing Here Component

---
+++ utils/nothing.tsx
@@ -0,0 +1,9 @@
+function NothingHere() {
+  return (
+    <div className=" my-8 text-para font-bold text-center">
+      There`s Nothing To Show Here
+    </div>
+  );
+}
+
+export default NothingHere;
