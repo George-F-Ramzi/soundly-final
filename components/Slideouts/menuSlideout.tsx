a7de6c79cd0f6bc73ad54f9426bd03444e42274b
George-F-Ramzi <109841377+George-F-Ramzi@users.noreply.github.com>
2023-07-27 08:29:53 +0300

    Added Logout Button

--- components/Slideouts/menuSlideout.tsx
+++ components/Slideouts/menuSlideout.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { RiCloseLine } from "react-icons/ri";
+import { deleteCookie } from "cookies-next";
 
 interface Prop {
   toggle: (value: boolean) => void;
@@ -8,46 +9,55 @@ interface Prop {
 
 export default function MenuSlideout({ toggle, id }: Prop) {
   return (
-    <div className="w-screen flex h-screen z-10 fixed right-0 top-0">
-      <div className="bg-black w-full h-full opacity-80"></div>
-      <div className="bg-[#191919]  min-w-[360px] p-8 h-full ">
-        <div className="flex mb-10 items-center justify-between">
-          <h2 className="text-para font-bold">Hi There!</h2>
+    <div className='w-screen flex h-screen z-10 fixed right-0 top-0'>
+      <div className='bg-black w-full h-full opacity-80'></div>
+      <div className='bg-[#191919]  min-w-[360px] p-8 h-full '>
+        <div className='flex mb-10 items-center justify-between'>
+          <h2 className='text-para font-bold'>Hi There!</h2>
           <RiCloseLine
             onClick={() => toggle(false)}
             width={24}
             height={24}
-            className="text-para cursor-pointer w-6 h-6"
+            className='text-para cursor-pointer w-6 h-6'
           />
         </div>
         <Link
           href={`/artist/${id}`}
           onClick={() => toggle(false)}
-          className="border rounded w-full flex items-center justify-center  h-12 border-default text-white font-bold"
+          className='border rounded w-full flex items-center justify-center  h-12 border-default text-white font-bold'
         >
           Profile
         </Link>
         <Link
           href={`/inbox`}
           onClick={() => toggle(false)}
-          className="border mt-4 rounded w-full flex items-center justify-center  h-12 border-default text-white font-bold"
+          className='border mt-4 rounded w-full flex items-center justify-center  h-12 border-default text-white font-bold'
         >
           Inbox
         </Link>
         <Link
           href={`/liked`}
           onClick={() => toggle(false)}
-          className="border mt-4 rounded w-full flex items-center justify-center  h-12 border-default text-white font-bold"
+          className='border mt-4 rounded w-full flex items-center justify-center  h-12 border-default text-white font-bold'
         >
           Liked
         </Link>
         <Link
           href={`/upload`}
           onClick={() => toggle(false)}
-          className="border mt-4 rounded w-full flex items-center justify-center  h-12 border-default text-white font-bold"
+          className='border mt-4 rounded w-full flex items-center justify-center  h-12 border-default text-white font-bold'
         >
           Upload
         </Link>
+        <button
+          onClick={() => {
+            deleteCookie("token");
+            window.location.reload();
+          }}
+          className='border mt-4 rounded w-full flex items-center justify-center  h-12 border-default text-white font-bold'
+        >
+          Log Out
+        </button>
       </div>
     </div>
   );
